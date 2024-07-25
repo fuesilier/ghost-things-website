81f91875c8618c6f87d2227b66c16fcd53e58dd0
missbildadkatt <michael.durden000@gmail.com>
2024-07-25 20:43:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const labels = document.querySelector('.current-year')
+  labels.textContent = new Date().getFullYear()
+
+  const images = document.querySelector('.thing-images')
+  for (let i = 0; i < 7; i++) {
+    let child = document.createElement('img')
+    child.setAttribute('src', `/images/photo_${i + 1}_2024-07-25_20-01-53.jpg`)
+    child.setAttribute('width', '200px')
+    images.appendChild(child)
+  }
+})
