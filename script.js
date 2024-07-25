265be0aa3fbb514b11553a81af289860faa1bffd
missbildadkatt <michael.durden000@gmail.com>
2024-07-25 21:17:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
   labels.textContent = new Date().getFullYear()
 
   const images = document.querySelector('.thing-images')
-  for (let i = 0; i < 7; i++) {
+  for (let i = 0; i < 6; i++) {
     let child = document.createElement('img')
     child.setAttribute('src', `/images/photo_${i + 1}_2024-07-25_20-01-53.jpg`)
     child.setAttribute('width', '200px')
